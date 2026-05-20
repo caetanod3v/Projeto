a9eb645a93cd3ed5ed901ef3320e3d6a38e7ca94
h3c <caetanolamego@gmail.com>
2026-05-20 00:15:25 -0300

    feat: redesign on fluxus wordmark v10

--- src/components/FluxusWordmark.jsx
+++ src/components/FluxusWordmark.jsx
@@ -4,9 +4,7 @@ export default function FluxusWordmark() {
   return (
     <div className="fluxus-wordmark" aria-label="Fluxus">
       <span className="fluxus-wordmark-text" aria-hidden="true">Fluxus</span>
-      <svg className="fluxus-wordmark-current" viewBox="0 0 180 34" preserveAspectRatio="none" aria-hidden="true">
-        <path d="M5 18 C28 9 42 9 62 18 S96 27 117 18 S151 9 175 18" />
-      </svg>
+      <span className="fluxus-wordmark-sweep" aria-hidden="true" />
     </div>
   );
 }
